50589990fc2eb8721902a7bf13aca390f95c876b
harshaldeshpande110 <harshal.deshpande@ilink-systems.com>
2025-08-20 02:05:51 +0530

    from parent ajs

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1> From Parent Component webhook</h1>
+      <h1> Parent Component Akshada</h1>
       <p>Count: {count}</p>
       <button onClick={() => setOtherState(!otherState)}>
         Toggle Other State

14c702e3fadd34d6df3c97fb66ecfd123321b16d
harshaldeshpande110 <harshal.deshpande@ilink-systems.com>
2025-08-20 02:02:13 +0530

    from parent

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Parent Component</h1>
+      <h1> From Parent Component</h1>
       <p>Count: {count}</p>
       <button onClick={() => setOtherState(!otherState)}>
         Toggle Other State
